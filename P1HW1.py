62220e73379611cc6d194a0862a3be9a6522f966
gomezj4488 <160150643+gomezj4488@users.noreply.github.com>
2024-02-15 13:50:15 -0500

    Update P1HW1.py

--- P1HW1.py
+++ P1HW1.py
@@ -1,3 +1,15 @@
-#Judith Gomez
-#02/15/2024
-#P1HW1
+# Judith Gomez
+# 02/15/2024
+# P1HW1
+# Using python's input and print functions
+
+user_num = int(input('Enter integer:\n'))
+user_num1 = int(input('Enter another integer:\n'))
+
+print("You entered: ", user_num )
+
+print( user_num, "squared is", user_num ** 3,"!!")
+
+print( user_num, "+", user_num1, "is", user_num + user_num1)
+
+print( user_num, "*", user_num1, "is", user_num * user_num1)
