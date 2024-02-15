55090ce1e9d954fbc2836af7ff2ed0365ba13a22
gomezj4488 <160150643+gomezj4488@users.noreply.github.com>
2024-02-15 16:04:36 +0000

    Created pratice file

---
+++ Practice.py
@@ -0,0 +1,9 @@
+#Hard coded - Value same everytime the program runs
+student1="Mary"
+
+#User input - value is assigned the user's input
+student2= input("Enter student's name: ")
+
+print("The first student's name is", student1 )
+
+print("The second student's name is", student2 )
