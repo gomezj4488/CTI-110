1d528d1d066e878f705c700fae0f15c70ee7b790
gomezj4488 <160150643+gomezj4488@users.noreply.github.com>
2024-02-15 14:04:12 -0500

    Update P1HW1.py

--- P1HW1.py
+++ P1HW1.py
@@ -4,11 +4,12 @@
 # Using python's input and print functions
 
 user_num = int(input('Enter integer:\n'))
-user_num1 = int(input('Enter another integer:\n'))
 
 print("You entered: ", user_num )
 
 print( user_num, "squared is", user_num ** 3,"!!")
+
+user_num1 = int(input('Enter another integer:\n'))
 
 print( user_num, "+", user_num1, "is", user_num + user_num1)
 
